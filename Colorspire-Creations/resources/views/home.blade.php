8a40387698fba41fdc9457ca13075dcc10bd896f
Rashid NChimunya <rashidmrahim344@gmail.com>
2026-09-04 00:03:06 +0200

    Created the home page, changed the route to home and removed the default welcome page

---
+++ Colorspire-Creations/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome - Colorspire Creations</title>
+</head>
+<body>
+    <h1>Colorspire Creations</h1>
+    <p>Creating beautiful and functional websites for businesses and individuals.</p>
+    <a href="#">Learn More</a>
+</body>
+</html>
